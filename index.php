041e612daa0ab19ff2c208df0a5f40f2d94951d6
Philip Cali <philip.cali@gmail.com>
2012-02-22 14:02:38 -0600

    User query foundation established

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $perpage = optional_param('perpage', DEFAULT_PAGE_SIZE, PARAM_INT);
 $roleid = optional_param('roleid', 0, PARAM_INT);
 $groupid = optional_param('groupid', 0, PARAM_INT);
 $meta = optional_param('meta', 'lastname', PARAM_TEXT);
-$sortdir = optional_param('dir', 'DESC', PARAM_TEXT);
+$sortdir = optional_param('dir', 'ASC', PARAM_TEXT);
 
 $silast = optional_param('silast', 'all', PARAM_TEXT);
 $sifirst = optional_param('sifirst', 'all', PARAM_TEXT);
@@ -62,44 +62,7 @@ if (empty($rolenames[$roleid])) {
 
 add_to_log($course->id, 'ues_people', 'view all', 'index.php?id='.$course->id, '');
 
-// UES section enrollment
 $all_sections = ues_section::from_course($course);
-
-$primary = has_capability('moodle/site:accessallgroups', $context);
-
-$enrolled_sections = function($class, $extra = array()) use ($all_sections) {
-    global $USER;
-
-    $sql = ues::where()
-        ->sectionid->in(array_keys($all_sections))
-        ->status->in(ues::ENROLLED, ues::PROCESSED)
-        ->userid->equal($USER->id);
-
-    foreach ($extra as $key => $value) {
-        $sql->{$key}->equal($value);
-    }
-
-    $sections = array();
-    $instances = $class::get_all($sql);
-
-    foreach ($instances as $instance) {
-        $sections[$instance->sectionid] = $all_sections[$instance->sectionid];
-    }
-
-    return $sections;
-};
-
-if ($primary) {
-    $sections = $all_sections;
-} else if (!$user->is_primary()) {
-    $sections = $enrolled_sections('ues_teacher', array('primary_flag' => 0));
-} else {
-    $sections = $enrolled_sections('ues_student');
-}
-
-if ($groupid) {
-    $active_sections = array($sections[$groupid]);
-}
 
 $meta_names = ues_user::get_meta_names();
 
@@ -109,6 +72,72 @@ $using_section_sort = $meta == 'section';
 $PAGE->set_title("$course->shortname: " . get_string('participants'));
 $PAGE->set_heading($course->fullname);
 $PAGE->set_pagetype('course-view-' . $course->format);
+
+$select = 'SELECT u.id, u.firstname, u.lastname, u.email, sec.sec_number, u.deleted,
+                  u.picture, u.imagealt, u.lang, u.timezone, stu.credit_hours';
+$joins = array('FROM {user} u');
+
+list($ccselect, $ccjoin) = context_instance_preload_sql('u.id', CONTEXT_USER, 'ctx');
+
+$select .= $ccselect;
+$joins[] = $ccjoin;
+
+list($esql, $params) = get_enrolled_sql($context, '', $groupid);
+
+$joins[] = "JOIN ($esql) e ON e.id = u.id";
+$joins[] = "LEFT JOIN " . ues_student::tablename('stu') . " ON u.id = stu.userid";
+$joins[] = "JOIN " . ues_section::tablename('sec') . " ON stu.sectionid = sec.id";
+
+if ($using_meta_sort) {
+    $meta_table = ues_user::metatablename('um');
+    $joins[] = 'LEFT JOIN ' . $meta_table. ' ON (
+        um.userid = u.id AND um.name = :metakey
+    )';
+    $params['metakey'] = $meta;
+}
+
+$from = implode("\n", $joins);
+
+$wheres = ues::where()
+    ->sectionid->in(array_keys($all_sections))
+    ->status->in(ues::ENROLLED, ues::PROCESSED);
+
+if ($sifirst != 'all') {
+    $wheres->firstname->starts_with($sifirst);
+}
+
+if ($silast != 'all') {
+    $wheres->lastname->starts_with($silast);
+}
+
+if ($roleid) {
+    $contextlist = get_related_contexts_string($context);
+
+    $sub = 'SELECT userid FROM {role_assignments}
+        WHERE roleid = :roleid AND context ' . $contextlist;
+
+    $wheres->id->raw("IN ($sub)");
+}
+
+$where = $wheres->is_empty() ? '' : 'WHERE ' . $wheres->sql(function($k) {
+    switch ($k) {
+        case 'sectionid': return 'stu.' . $k;
+        case 'status': return 'stu.' . $k;
+        default: return 'u.' . $k;
+    }
+});
+
+if ($using_meta_sort) {
+    $sort = 'ORDER BY um.value ' . $sortdir;
+} else if ($using_section_sort) {
+    $sort = 'ORDER BY sec.sec_number ' . $sortdir;
+} else {
+    $sort = 'ORDER BY u.' . $meta . ' ' . $sortdir;
+}
+
+$sql = "$select $from $where $sort";
+
+$users = $DB->get_records_sql($sql, $params, $page, $perpage);
 
 echo $OUTPUT->header();
 
@@ -125,7 +154,16 @@ $headers[] = $_s('credit_hours');
 
 $table->head = $headers;
 
-list($esql, $params) = get_enrolled_sql($context);
+foreach ($users as $user) {
+    $line = array();
+    $line[] = $OUTPUT->user_picture($user, array('courseid' => $id));
+    $line[] = fullname($user);
+    $line[] = $user->email;
+    $line[] = $user->sec_number;
+    $line[] = $user->credit_hours;
+
+    $table->data[] = new html_table_row($line);
+}
 
 echo html_writer::table($table);
 
