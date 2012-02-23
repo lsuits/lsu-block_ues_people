41cd6b75344e011676a6d9e2b6e3b7eea9ae0c3e
Philip Cali <philip.cali@gmail.com>
2012-02-23 08:20:37 -0600

    Admin choice on defined outputs.

--- lang/en/block_ues_people.php
+++ lang/en/block_ues_people.php
@@ -1,3 +1,9 @@
 <?php
 
 $string['pluginname'] = 'UES People';
+
+$string['sec_number'] = 'Section #';
+$string['credit_hours'] = 'Credit Hours';
+
+$string['outputs'] = 'Meta Outputs';
+$string['outputs_desc'] = 'The selected meta fields will be the only fields users can filter upon.';
--- lib.php
+++ lib.php
@@ -23,17 +23,37 @@ abstract class ues_people {
         return $DB->get_records_sql('SELECT * FROM {role} WHERE ' . $role_sql);
     }
 
+    public static function defaults() {
+        return explode(',', get_config('block_ues_people', 'outputs'));
+    }
+
     public static function outputs() {
-        $defaults = array('sec_number', 'credit_hours');
-        $meta_names = array_merge($defaults, ues_user::get_meta_names());
+        $defaults = self::defaults();
+
+        $internal = array('sec_number', 'credit_hours');
+        $meta_names = array_merge($internal, ues_user::get_meta_names());
+
+        $_s = ues::gen_str('block_ues_people');
 
         $outputs = array();
+
         foreach ($meta_names as $meta) {
-            $outputs[$meta] = new ues_people_element_output($meta);
+            // Admin choice on limits
+            if (!in_array($meta, $defaults)) {
+                continue;
+            }
+
+            $element = in_array($meta, $internal) ?
+                new ues_people_element_output($meta, $_s($meta)) :
+                new ues_people_element_output($meta);
+
+            $outputs[$meta] = $element;
         }
 
         $data = new stdClass;
         $data->outputs = $outputs;
+
+        // Plugin interference
         events_trigger('ues_people_outputs', $data);
 
         return $data->outputs;
@@ -42,14 +62,19 @@ abstract class ues_people {
 
 class ues_people_element_output {
     var $name;
+    var $field;
 
-    function __construct($name) {
+    function __construct($field, $name = '') {
+        $this->field = $field;
+        if (empty($name)) {
+            $name = $field;
+        }
         $this->name = $name;
     }
 
     function format($user) {
-        if (isset($user->{$this->name})) {
-            return $user->{$this->name};
+        if (isset($user->{$this->field})) {
+            return $user->{$this->field};
         } else {
             return '';
         }
