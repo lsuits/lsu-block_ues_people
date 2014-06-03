4aa6dffa9ffb0281c738cb08dbe2ba41c0437559
Jason Peak <jpeak5@lsu.edu>
2014-06-03 08:53:56 -0500

    insert license; add log event class in order to replace add_to_log fn

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ *
+ * @package    block_ues_people
+ * @copyright  2014 Louisiana State University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once '../../config.php';
 require_once $CFG->dirroot . '/enrol/ues/publiclib.php';
@@ -104,7 +126,11 @@ $isseparategroups = (
     !has_capability('moodle/site:accessallgroups', $context)
 );
 
-add_to_log($course->id, 'ues_people', 'view all', 'index.php?id='.$course->id, '');
+$event = \block_ues_people\event\all_ues_logs_viewed::create(array(
+    'context' => $PAGE->context,
+));
+$event->add_record_snapshot('course', $PAGE->course);
+$event->trigger();
 
 $meta_names = ues_people::outputs($course);
 
